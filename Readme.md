5bd15a11c2a0a2c24a939e91b08aa2efbaf0efaa
Shalu <shalu.handysolver@gmail.com>
2023-12-08 23:52:29 +0545

    Episode 1

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Namaste React
+
+<!-- ******************** Episode 1 ******************** -->
+# What is react?
+# react-development.js => core of react
+# react-dom.development.js => react library useful for dom manipulation
+# react-dom => bridge to the react & browser
+# DOM Manipulation
+# Crossorigin 
+# createElement => creact react element(javascript object)
+# props => children + attributes of react element
+# root.render(heading) => render puts the element on browser => ReactElemet(javascript object) => HTML(Browser understands)
+# Nested structure in React
+# Siblings inside nested structure
+# React is a Javascript library. It gives some helper methods to use and develop our application fast. We can do that in small portion as well.
--- app.js
+++ app.js
@@ -1,63 +1,32 @@
+import React from "react"
+import ReactDOM from "react-dom/client"  //
 
 
 
-// ************ Using React **************
-    /*** Example
-     * <div id="parent">
-     *      <div id="child1">
-     *          <h1>H1 tag</h1>
-     *          <h2>H2 tag</h2>
-     *      </div>
-     *      <div id="child2">
-     *          <h1>H1 tag</h1>
-     *          <h2>H2 tag</h2>
-     *      </div>
-     * </div>
-    ***/
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
+// ************ Basic React ************
+
+// ReactElemet(javascript object) => HTML(Browser understands)
+const parent = React.createElement("div", { }, 
     [
-        React.createElement(
-            "div", 
-            {id: "child1"}, 
-            [
-                React.createElement(
-                    "h1",{}, "H1 tag"
-                ),
-                React.createElement(
-                    "h2",{}, "H2 tag"
-                )
-            ],
-        ),
-        React.createElement(
-            "div", 
-            {id: "child2"}, 
-            [
-                React.createElement(
-                    "h1",{}, "H1 tag"
-                ),
-                React.createElement(
-                    "h2",{}, "H2 tag"
-                )
-            ],
-        )
-    ],
-);       
+        React.createElement("h1", { }, "Hello React1!"),
+        React.createElement("h2", { }, "Hello React2!")
+    ]
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
+console.log(parent); // javascript object
 root.render(parent);
 
 
-// const heading = React.createElement("h1", {id: "heading", attr: "attribute"}, "Hello World from React!");       
+// ***************************
+// const h1 = React.createElement("h1", {}, "Hello React!");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(root);
-// root.render(heading);
+// root.render(h1);
 
 
-// *********** Pure JS **********
-        // let h1 = document.createElement("h1");
-        // h1.innerHTML = "Hello World!";
-        // let root = document.getElementById("root");
-        // root.appendChild(h1);
+
+// ************* Pure Javascript ************
+// const h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World!";
+// const root = document.getElementById("root");
+// root.appendChild(h1);
